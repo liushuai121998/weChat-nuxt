3dc97c95044ed5df11a4d8894443ca6989217393
liushuai121998 <liushuai121998@gmail.com>
2018-09-27 14:58:16 +0800

    wechat

---
+++ pages/user/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Welcome!</h1>
+        <nuxt-link to="/user/one">to one</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
---
+++ pages/user/one.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hi from {{name}}</h1>
+        <nuxt-link to="/user">to user</nuxt-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        asyncData () {
+            return {
+                name: process.static ? 'static' : (process.server ? 'server' : 'client')
+            }
+        },
+        head: {
+            title: 'User One'
+        }
+    }
+</script>
